75a52f739d7b062f7dc13de59ae3f2307669b511
l <mx24xnpi4@mozmail.com>
2022-05-14 22:33:12 +0300

    feat: subscribe input value to a store

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   export let greeting: string;
   import Input from './Input.svelte';
+  import { query } from './stores';
 </script>
 
 <main class="max-w-xs m-auto">
   <h1 class="text-pink-400 text-center text-4xl">{greeting}</h1>
   <Input name="title" />
+  <p>{$query}</p>
 </main>
 
 <style>
--- src/Input.svelte
+++ src/Input.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import { query } from './stores';
 
   export let name = 'input';
   let ref = null;
@@ -14,6 +15,7 @@
     class="border-2 rounded-md px-2 py-1 text-base"
     type="text"
     name="{name}"
+    bind:value="{$query}"
     bind:this="{ref}"
   />
 </div>
